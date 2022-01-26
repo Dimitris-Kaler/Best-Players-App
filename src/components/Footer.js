9b0db7ba2010dd7d04c89331d3847774878648ad
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-01-26 14:13:30 +0200

    className instead class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,12 @@ const Footer=()=>{
         <div id="footer">
             <p>Dimitris Kalerantes</p>
             <div id="media">
-            <a href="https://www.facebook.com/" class="fab fa-facebook-f"></a>
-            <a href="https://www.instagram.com/" class="fab fa-instagram"></a>
-            <a href="https://www.linkedin.com/in/dimitris-kalerantes/" class="fab fa-linkedin"></a>
+            <a href="https://www.facebook.com/" className="fab fa-facebook-f"></a>
+            <a href="https://www.instagram.com/" className="fab fa-instagram"></a>
+            <a href="https://www.linkedin.com/in/dimitris-kalerantes/" className="fab fa-linkedin"></a>
             </div>
         </div>
     )
-
-
-
-
-
 
 }
 

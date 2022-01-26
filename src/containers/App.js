0a3994abdc9f24bd4730d6aeed12a245f8094252
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-01-26 03:34:22 +0200

    clean the code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,12 @@
 import React,{Component} from 'react';
 import { BrowserRouter, Route,Routes, Switch } from 'react-router-dom'
-import CardList from '../components/CardList.js';
-import SearchBox from "../components/SearchBox.js";
-import Scroll from '../components/Scroll.js'
 import './App.css';
-import {FootballPlayers} from '../FootballPlayers.js';
 import Stats from '../components/Stats.js';
 import Home from '../components/Home'
 import Footer from '../components/Footer'
 
 
-// class App extends Component{
-//   constructor(){
-//     super()
-//     this.state={
-//       footballPlayers:FootballPlayers,
-//       searchField:''
-//     }
-//   }
-//   onSearchChange=(event)=>{
-//     this.setState({searchField:event.target.value})
 
-//   }
-//   render(){ 
-//     const filteredPlayers=this.state.footballPlayers.filter(player=>{
-     
-//       return player.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-//     })
 
     const App=()=>{
     

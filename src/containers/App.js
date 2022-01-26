8f40ac603d23733721604a3616b8c5c7b5f0fa11
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-01-27 00:19:35 +0200

    app

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,7 +11,7 @@ import Footer from '../components/Footer'
     const App=()=>{
     
     return(
-    <div className="App-div">
+    <div className="App">
       <BrowserRouter>   
         <Routes>
           <Route exact path="/" element={<Home/>}/>

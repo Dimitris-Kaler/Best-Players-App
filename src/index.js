65f8e9a403d5cc07322ee6ff91f20c2591d31ad5
Dimitris-Kaler <74980408+Dimitris-Kaler@users.noreply.github.com>
2022-01-27 01:02:00 +0200

    browserRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 // import Footer from './components/Footer'
 
 ReactDOM.render(
- <HashRouter>
+ <BrowserRouter basename='/'>
     <App />
-</HashRouter>
+</BrowserRouter>
  ,
   document.getElementById('root')
 );
